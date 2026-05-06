565e6bc36c0c0933b79b657b2c38452d03d774c3
HFerZHY <3164365889@qq.com>
2026-05-05 22:41:59 -0700

    + Initial addition of character generation base class defining recolor textures and color palettes + Add test animation

---
+++ Assets/Scripts/Character/CharacterBase.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CharacterBase : ScriptableObject
+{
+    public Texture2D lineArtTexture;
+    public Texture2D[] colorTextures;
+}
---
+++ Assets/Scripts/Data/ColorPaletteSet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewPaletteSet", menuName = "Data/New Palette Set")]
+public class ColorPaletteSet : ScriptableObject
+{
+    [Min(1)]
+    public int paletteSize = 2;
+
+    public ColorPalette[] palettes;
+    
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        for (int i = 0; i < palettes.Length; i++)
+        {
+            if (palettes[i].colors == null) continue;
+
+            if (palettes[i].colors.Length != paletteSize)
+            {
+                Debug.LogWarning(
+                    $"[ColorPaletteSet] '{name}': palette '{palettes[i].name}' " +
+                    $"(index {i}) has {palettes[i].colors.Length} colors but expected {paletteSize}.",
+                    this
+                );
+            }
+        }
+    }
+#endif
+}
+
+[Serializable]
+public struct ColorPalette
+{
+    public string name;
+    public Color[] colors;
+}
